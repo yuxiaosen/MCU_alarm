48127a667f7676d872cb33ab491b468acac3059c
yuxiaosen <46220959+yuxiaosen@users.noreply.github.com>
2019-01-05 22:06:34 +0800

    Create sys.h

---
+++ sys.h
@@ -0,0 +1,19 @@
+#ifndef __SYS_H
+#define __SYS_H
+#include "reg51.h"
+#include "intrins.h"
+
+#ifndef uchar 
+#define uchar unsigned char
+#endif
+
+#ifndef uint 
+#define uint unsigned int
+#endif
+
+#ifndef ulong 
+#define ulong unsigned long
+#endif
+
+
+#endif
